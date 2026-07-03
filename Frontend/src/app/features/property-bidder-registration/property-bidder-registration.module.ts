39836fc8bf7cbb051ff7d9844894c1895884167a
Vikas <vikas.kumar@ssbi.in>
2026-07-03 11:17:06 +0530

    created the property bidder registration component and its model

---
+++ Frontend/src/app/features/property-bidder-registration/property-bidder-registration.module.ts
@@ -0,0 +1,22 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { RouterModule } from '@angular/router';
+
+import { SharedModule } from '../../shared/shared.module';
+import { PropertyBidderRegistrationComponent } from './property-bidder-registration.component';
+
+@NgModule({
+  declarations: [PropertyBidderRegistrationComponent],
+  imports: [
+    CommonModule,
+    SharedModule,
+
+    RouterModule.forChild([
+      {
+        path: 'property-bidder-registration',
+        component: PropertyBidderRegistrationComponent
+      }
+    ])
+  ]
+})
+export class PropertyBidderRegistrationModule {}
