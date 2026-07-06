2d72a3c84f83731124f0748d444cde0dc5c59590
Deepak Goel <deepakgoel@weexcelindia.com>
2026-07-06 15:19:44 +0530

    fix: angular budget issue + added frontend design

--- Backend/Data/ApplicationDbContext.cs
+++ Backend/Data/ApplicationDbContext.cs
@@ -10,6 +10,8 @@ public class ApplicationDbContext : IdentityDbContext<ApplicationUser>
     {
     }
 
+    public DbSet<ApplicationUsers> ApplicationUsers { get; set; }
+
     protected override void OnModelCreating(ModelBuilder builder)
     {
         base.OnModelCreating(builder);
