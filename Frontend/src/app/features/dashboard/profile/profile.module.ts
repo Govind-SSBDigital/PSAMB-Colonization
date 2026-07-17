338dff39515d8f7878397d1e420f756d7df2d56a
Vikas <vikas.kumar@ssbi.in>
2026-07-17 13:56:16 +0530

    feat: Implement user profile management with avatar upload and form validation

--- Frontend/src/app/features/dashboard/profile/profile.module.ts
+++ Frontend/src/app/features/dashboard/profile/profile.module.ts
@@ -2,11 +2,13 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterModule } from '@angular/router';
 import { Profile } from './profile';
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
-  declarations: [],
+  declarations: [Profile],
   imports: [
     CommonModule,
+    FormsModule,
     RouterModule.forChild([
           {
             path: '',
