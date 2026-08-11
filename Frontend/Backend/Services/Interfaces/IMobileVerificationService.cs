291e6a0b5506d32919f83b1da1b716f9efef1f7d
ERP4CGC <shriprakash.j4812@cgcuniversity.in>
2026-08-11 11:49:54 +0530

    commit

--- Frontend/Backend/Services/Interfaces/IMobileVerificationService.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace Backend.Services.Interfaces
-{
-    public interface IMobileVerificationService
-    {
-
-        Task<bool> SendSmsAsync(string mobileNo, string message, string otp, long applicantId);
-        Task<bool> VerifyOtpAsync(string mobileNo, string otp);
-    }
-}
