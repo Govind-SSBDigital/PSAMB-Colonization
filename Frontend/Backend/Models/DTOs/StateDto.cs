a02f5aafeb925a7228cceaf6748cf7ccbd82e4a1
Vikas <vikas.kumar@ssbi.in>
2026-08-11 15:22:45 +0530

    removed duplicate backend and frontend folder in frontend folder

--- Frontend/Backend/Models/DTOs/StateDto.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace Backend.Models.Dtos
-{
-    public class StateDto
-    {
-        public int StateId { get; set; }
-        public string StateName { get; set; } = string.Empty;
-    }
-}
--- Frontend/Backend/Services/Interfaces/ISendCredUserService.cs
+++
@@ -1,9 +0,0 @@
-﻿using Backend.Models.DTOs;
-
-namespace Backend.Services.Interfaces
-{
-    public interface ISendCredUserService
-    {
-        Task<bool> SendCredentialsAsync(SendCredModel model);
-    }
-}
